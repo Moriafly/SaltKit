d14a964d5509455f89d823884cf63ae6cc729b2a
Moriafly <moriafly@163.com>
2023-10-16 19:03:34 +0800

    新的 centerCrop 函数实现以及测试代码，添加注解等

---
+++ kit/src/androidTest/java/com/moriafly/salt/kit/BitmapKitTest.kt
@@ -0,0 +1,38 @@
+package com.moriafly.salt.kit
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BitmapKitTest {
+
+    @OptIn(UnstableSaltKitApi::class)
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.moriafly.salt.kit.test", appContext.packageName)
+
+        val b1 = BitmapKit.createBitmapFullColor(500, 1, Color.WHITE, Bitmap.Config.ARGB_8888)
+        val bn1 = BitmapKit.centerCrop(b1, 500, 500)
+        println("bn1 w = ${bn1.width} h = ${bn1.height}")
+
+        val b2 = BitmapKit.createBitmapFullColor(500, 300, Color.WHITE, Bitmap.Config.ARGB_8888)
+        val bn2 = BitmapKit.centerCrop(b2, 500, 500)
+        println("bn2 w = ${bn2.width} h = ${bn2.height}")
+
+        val b3 = BitmapKit.createBitmapFullColor(1000, 3000, Color.WHITE, Bitmap.Config.ARGB_8888)
+        val bn3 = BitmapKit.centerCrop(b3, 500, 500)
+        println("bn3 w = ${bn3.width} h = ${bn3.height}")
+
+        val b4 = BitmapKit.createBitmapFullColor(20000, 15000, Color.WHITE, Bitmap.Config.RGB_565)
+        val bn4 = BitmapKit.centerCrop(b4, 500, 500)
+        println("bn4 w = ${bn4.width} h = ${bn4.height}")
+    }
+
+}
---
+++ kit/src/main/java/com/moriafly/salt/kit/BitmapKit.kt
@@ -0,0 +1,105 @@
+@file:Suppress("UNUSED")
+
+/**
+ * SaltKit
+ * Copyright (C) 2023 Moriafly
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package com.moriafly.salt.kit
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.Paint
+
+object BitmapKit {
+
+    private const val PAINT_FLAGS = Paint.DITHER_FLAG or Paint.FILTER_BITMAP_FLAG
+
+    private val defaultPaint: Paint = Paint(PAINT_FLAGS)
+
+    /**
+     * Create a color bitmap
+     */
+    @UnstableSaltKitApi
+    fun createBitmapFullColor(
+        width: Int,
+        height: Int,
+        color: Int,
+        config: Bitmap.Config
+    ): Bitmap {
+        val bitmap = Bitmap.createBitmap(width, height, config)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(color)
+        return bitmap
+    }
+
+    /**
+     * Center crop bitmap
+     * This api build with Glide/TransformationUtils.java and ImageView/Bitmap.createScaledBitmap
+     */
+    @UnstableSaltKitApi
+    fun centerCrop(
+        bitmap: Bitmap,
+        width: Int,
+        height: Int
+    ): Bitmap {
+        if (bitmap.width == width && bitmap.height == height) {
+            return bitmap
+        }
+        // From ImageView/Bitmap.createScaledBitmap
+        val scale: Float
+        val dx: Float
+        val dy: Float
+        val matrix = Matrix()
+        if (bitmap.width * height > width * bitmap.height) {
+            scale = height.toFloat() / bitmap.height.toFloat()
+            dx = (width - bitmap.width * scale) * 0.5f
+            dy = 0f
+        } else {
+            scale = width.toFloat() / bitmap.width.toFloat()
+            dx = 0f
+            dy = (height - bitmap.height * scale) * 0.5f
+        }
+
+        matrix.setScale(scale, scale)
+        matrix.postTranslate((dx + 0.5f).toInt().toFloat(), (dy + 0.5f).toInt().toFloat())
+
+        val result = Bitmap.createBitmap(width, height, bitmap.config ?: Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(result)
+        canvas.drawBitmap(bitmap, matrix, defaultPaint)
+        canvas.setBitmap(null)
+        return result
+    }
+
+    /**
+     * Cut bitmap into a square, and take only the middle part of a rectangular bitmap
+     */
+    fun square(bitmap: Bitmap): Bitmap {
+        val width = bitmap.width
+        val height = bitmap.height
+        if (width == height) {
+            return bitmap
+        }
+        val clipWidth = if (width > height) height else width
+        val x = (width - clipWidth) / 2
+        val y = (height - clipWidth) / 2
+        return Bitmap.createBitmap(bitmap, x, y, clipWidth, clipWidth)
+    }
+
+}
+
+@UnstableSaltKitApi
+fun Bitmap.centerCrop(width: Int, height: Int): Bitmap = BitmapKit.centerCrop(this, width, height)
+
+fun Bitmap.toSquare(): Bitmap = BitmapKit.square(this)
