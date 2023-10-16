eebdadb22a3ef6c601d6a953fef2859b0a2e8b99
Moriafly <moriafly@163.com>
2023-10-16 19:02:47 +0800

    min sdk 23

--- kit/build.gradle.kts
+++ kit/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
